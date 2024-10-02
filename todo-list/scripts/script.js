afa71163c2be63f862ecf717817d2dd5e6226449
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2024-10-01 22:02:25 -0300

    feat: add electron

--- todo-list/scripts/script.js
+++ todo-list/scripts/script.js
@@ -1,10 +1,10 @@
 var tarefas = [];
 
-function adicionarTarefa(){
+function adicionarTarefa() {
     const input = document.getElementById("tarefa-text");
     const tarefaTexto = input.value.trim();
 
-    if(tarefaTexto === ''){
+    if (tarefaTexto === '') {
         alert("VOCÊ TENTOU ADICIONAR UMA TAREFA SEM TEXTO");
         return;
     }
@@ -16,6 +16,7 @@ function adicionarTarefa(){
     }
 
     tarefas.push(novaTarefa);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
     render()
     input.value = "";
     input.focus();
@@ -25,9 +26,9 @@ function render() {
     const listaTarefas = document.getElementById("lista-tarefa");
     listaTarefas.innerHTML = "";
 
-    for(var i = 0; i < tarefas.length; i++){
+    for (var i = 0; i < tarefas.length; i++) {
         const li = document.createElement("li");
-        if(tarefas[i].completed === true){
+        if (tarefas[i].completed === true) {
             li.classList.add("completed");
         }
 
@@ -37,14 +38,17 @@ function render() {
         const concluir = document.createElement("button");
         concluir.textContent = tarefas[i].completed ? "Desmarcar" : "Concluir";
         concluir.classList.add("check");
+        concluir.setAttribute("onclick", `trocaConcluir(${tarefas[i].id})`);
 
         const edit = document.createElement("button");
         edit.textContent = "Editar";
         edit.classList.add("edit");
+        edit.setAttribute("onclick", `editarTarefa(${tarefas[i].id})`)
 
         const deletar = document.createElement("button");
         deletar.textContent = "Deletar";
-        concluir.classList.add("delete");
+        deletar.classList.add("delete");
+        deletar.setAttribute("onclick", `deletarTarefa(${tarefas[i].id})`)
 
         const div = document.createElement("div");
 
@@ -56,5 +60,44 @@ function render() {
         li.appendChild(div);
 
         listaTarefas.appendChild(li);
+    }
+}
+
+function trocaConcluir(id) {
+    const index = tarefas.findIndex(tarefa => tarefa.id === id);
+    const valorAtual = tarefas[index].completed;
+    tarefas[index].completed = !valorAtual;
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    render();
+}
+
+function editarTarefa(id) {
+    const index = tarefas.findIndex(tarefa => tarefa.id === id);
+    const novoTextoTarefa = prompt('Edite a tarefa', tarefas[index].text);
+
+    if (novoTextoTarefa !== null && novoTextoTarefa.trim() !== '') {
+        tarefas[index].text = novoTextoTarefa;
+        localStorage.setItem("tarefas", JSON.stringify(tarefas))
+        render();
+    }
+}
+
+function deletarTarefa(id){
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    render();
+}
+
+function addPeloEnter(evento){
+    if(evento.key === 'Enter'){
+        adicionarTarefa();
+    }
+}
+
+function carregarTarefas() {
+    const tarefasLocalStore = localStorage.getItem("tarefas");
+    if(tarefasLocalStore){
+        tarefas = JSON.parse(tarefasLocalStore);
+        render();
     }
 }
