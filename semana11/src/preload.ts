47b9070f9801d1df44a5959f73c2dfd1ba67410a
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2024-11-04 21:57:18 -0300

    feat: add delete by id

---
+++ semana11/src/preload.ts
@@ -0,0 +1,9 @@
+import { contextBridge, ipcRenderer } from "electron";
+import Veiculo from "./entity/Veiculo";
+
+contextBridge.exposeInMainWorld('bancoAPI', {
+    createVeiculo: async (veiculo: Veiculo) => await ipcRenderer.invoke('create', veiculo),
+    findAll: async () => await ipcRenderer.invoke('findAll'),
+    findById: async (id: string) => await ipcRenderer.invoke('findById', id),
+    deletarVeiculo: async (id: string) => await ipcRenderer.invoke('deletarVeiculo', id)
+})
